c5091b17d78630ab2afe9444dc067280665946f0
Asif@edulab <asif@edulab.in>
2023-02-06 16:01:11 +0530

    React table

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Table from './Table';
+
+const data = [
+  {
+    fileName: 'File 1',
+    members: 'John, Jane',
+    lastEdit: '01/01/2022',
+    size: '1MB',
+  },
+  {
+    fileName: 'File 2',
+    members: 'Mike, Sarah',
+    lastEdit: '02/01/2022',
+    size: '2MB',
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Table data={data} />
     </div>
   );
 }
